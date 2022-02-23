6d75e075caa6d170f40ee3a8cc8551e42a0d3c05
Gapragapr <gapragapr3@mail.ru>
2022-02-23 17:05:37 +0600

    update code & tests

--- src/js/__tests__/main-tests.js
+++ src/js/__tests__/main-tests.js
@@ -1,41 +1,19 @@
-import Validator from '../main';
+import Validator from '../main'
 
-test.each(['1Bill', '-Bill', '_Bill', '!Bill'])(
-  'should return an error for entering numbers, underscores, and dashes at the beginning of the string',
-  (userName) => {
-    const validator = new Validator(userName);
+test ('ckeck regEx work', () => {
+  const str = new Validator('qwerty');
 
-    const textErr = 'Имя не должно начинаться цифрами, символами подчёркивания или тире.';
-    expect(() => validator.validateUsername()).toThrowError(new Error(textErr));
-  },
-);
+  const expected = true;
+  const received = str.validateUsername();
 
-test.each(['Bill-', 'Bill1', 'Bill_'])(
-  'should return an error when entering numbers, underscores and dashes at the end of a line',
-  (userName) => {
-    const validator = new Validator(userName);
+  expect(received).toBe(expected)
+})
 
-    const textErr = 'Имя не должно заканчиваться цифрами, символами подчёркивания или тире.';
-    expect(() => validator.validateUsername()).toThrowError(new Error(textErr));
-  },
-);
+test ('ckeck regEx func err', () => {
+  const str = () => {
+    let strr = new Validator('123qwertt');
+    return strr.validateUsername();
+  }
 
-test('should return an error for entering more than 3 digits in a row', () => {
-  const validator = new Validator('Bi1994ll');
-
-  const textErr = 'Имя не должно содержать подряд более трёх цифр.';
-  expect(() => validator.validateUsername()).toThrowError(new Error(textErr));
-});
-
-test('should return an error when writing cyrillic', () => {
-  const validator = new Validator('Валентин');
-
-  const textErr = 'Имя не должно содержать Кириллицы.';
-  expect(() => validator.validateUsername()).toThrowError(new Error(textErr));
-});
-
-test('should return the user name', () => {
-  const validator = new Validator('Bill');
-
-  expect(validator.validateUsername()).toBe('Bill');
-});
+  expect(str).toThrow('Invalid username')
+})
--- src/js/main.js
+++ src/js/main.js
@@ -4,22 +4,16 @@ export default class Validator{
     }
 
     validateUsername() {
-        if (/^[\d\-^!@#$%^&*()_]/.test(this.userName)) {
-          throw new Error('Имя не должно начинаться цифрами, символами подчёркивания или тире.');
-        }
-    
-        if (/[\d\-^!@#$%^&*()_]$/.test(this.userName)) {
-          throw new Error('Имя не должно заканчиваться цифрами, символами подчёркивания или тире.');
-        }
-    
-        if (/\d{4}/.test(this.userName)) {
-          throw new Error('Имя не должно содержать подряд более трёх цифр.');
-        }
-    
-        if (/[А-Яа-яЁё]/.test(this.userName)) {
-          throw new Error('Имя не должно содержать Кириллицы.');
-        }
-    
-        return this.userName;
+      const regEx = /^[a-zA-Z]+[\w-]+[a-zA-Z]$/;
+      const regEx2 = /\d{3}/;
+      if (!regEx.test(this.userName) || regEx2.test(this.userName)){
+        throw new Error('Invalid username')
       }
+
+      return true
+    }
 }
+
+
+const name = new Validator('qwety');
+name.validateUsername()
